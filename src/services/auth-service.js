aa77805020a629c1215b475af992a5440506d43a
mrussell246 <montalbano.r@northeastern.edu>
2023-04-20 11:31:24 -0400

    fix login cookie

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+const api = axios.create({
+  withCredentials: true,
+});
+
 
 const DB_URL = 'https://webdevfinal-8nu2.onrender.com/api'
 
@@ -10,14 +14,14 @@ export const register = async (newUser) => {
 }
 
 export const login = async (credentials) => {
-    const response = await axios.post(`${DB_URL}/login`, credentials);
+    const response = await api.post(`${DB_URL}/login`, credentials);
     const user = response.data;
 
     return user
 }
 
 export const getCurrentUser = async () => {
-    const response = await axios.get(`${DB_URL}/get-current-user`);
+    const response = await api.get(`${DB_URL}/get-current-user`);
     const user = response.data;
 
     return user

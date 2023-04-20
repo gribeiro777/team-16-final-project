48358df381684d4ca6bb545c723d49573c83f784
mrussell246 <montalbano.r@northeastern.edu>
2023-04-20 18:02:37 -0400

    basic track details

--- src/track/track-info.js
+++ src/track/track-info.js
@@ -9,15 +9,27 @@ const TrackInfo = (props) => {
         dispatch(getTrackThunk(props.tid))
     }, [])
 
-    return <div>{currentTrack ?
-        <div>
-            <p>{currentTrack.name}</p>
-            <p>{currentTrack.artists.map(artist => artist.name)}</p>
-            <p>{`${Math.floor(currentTrack.duration_ms / 60000)}:${Math.trunc(currentTrack.duration_ms / 1000 % 60)}`}</p>
-            <p>{currentTrack.album.name}</p>
-            <img width={'36px'} height={'36px'} src={currentTrack.album.images[0].url}></img>
+    return (
+    <div className='mt-4'>
+        {currentTrack ?
+        <div className="row">
+            <div className='col-3'>
+                <img src={currentTrack.album.images[0].url}
+                     className='img-fluid' alt='Album'></img>
+            </div>
+            <div className='col-9 color-first d-flex flex-column justify-content-end'>
+                <h1 className='fw-bold' style={{ fontSize: '6vw' }}>{currentTrack.name}</h1>
+                <h3 className='mb-4'>Album: {currentTrack.album.name}</h3>
+                <span>
+                {currentTrack.artists.map(artist => `${artist.name} ● `)}
+                {currentTrack.album.release_date.slice(0, 4)} ● { }
+                {`${Math.floor(currentTrack.duration_ms / 60000)}:${Math.trunc(currentTrack.duration_ms / 1000 % 60)}`}
+                </span>
+            </div>
         </div>
-        : 'invalid song id'}</div>
+        : 'invalid song id'}
+    </div>
+    );
 }
 
 export default TrackInfo

faebcc34537e850c84b678f8eea28171205ad9e2
mrussell246 <montalbano.r@northeastern.edu>
2023-04-20 12:50:12 -0400

    add dynamic login/profile button based on logged in status

--- src/navigation/navigation-bar.js
+++ src/navigation/navigation-bar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logoutThunk } from "../thunks/auth-thunks";
 
 const NavigationBar = () => {
+    const { currentUser } = useSelector((state) => state.authData);
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -22,6 +24,8 @@ const NavigationBar = () => {
             <Link className='text-decoration-none text-black' to='/'><h1 className='px-5'>Website</h1></Link>
             <Link className='text-decoration-none text-black' to='/'><h2>Personal</h2></Link>
             <div className='d-flex align-items-center position-absolute end-0 me-3 h-100 '>
+            {!currentUser && <Link className='text-decoration-none text-black' to='/login'><h2>Login</h2></Link>}
+            {currentUser && 
                 <div class="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img alt='profile' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' width='40px' className='img-fluid rounded-circle'/>
@@ -30,7 +34,7 @@ const NavigationBar = () => {
                         <li><Link className='dropdown-item' to='/profile'>Profile</Link></li>
                         <li><a class="dropdown-item" href="#" onClick={logout}>Logout</a></li>
                     </ul>
-                </div>
+                </div>}
             </div>
         </div>
     );

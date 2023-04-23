40543f8e339a680fe8f254172accc6ca8365b0e7
gribeiro777 <ribeiro.ga@husky.neu.edu>
2023-04-22 21:47:48 -0400

    merged main

--- src/home/post-list/index.js
+++ src/home/post-list/index.js
@@ -3,20 +3,13 @@ import {useDispatch, useSelector} from "react-redux";
 import PostItem
     from "./post-list-item";
 import {findPostsThunk} from "../../thunks/post-thunks";
-import {findUsersByUsernameThunk} from "../../thunks/user-thunks";
 
 const PostsList = ({user}) => {
-    const {posts, loadingPostUsers} = useSelector(state => state.postData)
+    const {posts} = useSelector(state => state.postData)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(findPostsThunk(user ? user.username : null))
     }, [user])
-
-    useEffect(() => {
-        if (posts.length && !loadingPostUsers) {
-            dispatch(findUsersByUsernameThunk(posts.map(post => post.username)))
-        }
-    }, [posts])
 
     return <div className="text-white">
         <ul className="list-group rounded-0">
--- src/home/post-list/post-list-item.js
+++ src/home/post-list/post-list-item.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 
 const PostItem = (
@@ -19,30 +18,28 @@ const PostItem = (
         }
     }
 ) => {
-    const {postUsers} = useSelector(state => state.userData)
 
     return(
         <li className="list-group-item main-color text-white border-start-0 border-end-0 border-white border-sm-3">
             <div className="row">
                 <div className="col-3">
+                <Link className="text-decoration-none" to={`/tracks/${post.spotifyID}`}>
                     <div className="row">
                         <div className="col-auto d-flex flex-wrap align-items-center">
-                            <Link to={`/tracks/${post.spotifyID}`}>
                                 <img className="rounded-4 " height={64} src={post.albumArt}/>
-                            </Link>
                         </div>
                         <div className="col">
-                            <div className="row">{post.songTitle}</div>
+                            <div className="row text-white">{post.songTitle}</div>
                             <div className="row time">{post.artists.map(artist => artist.name).join(', ')}</div>
                         </div>
                     </div>
+                    </Link>
                 </div>
                 <div className="col-9">
-                    <div><b>{postUsers.length ?
-                        <Link to={`/profile/${postUsers.find(entry => entry.username === post.username)._id}`} style={{textDecoration: 'none'}}>
+                    <div><b>
+                        <Link to={`/profile/${post.username}`} style={{textDecoration: 'none'}}>
                             <span className="username">{post.username}</span>
-                        </Link> :
-                        post.username}</b> <span>&#183;</span> <span className="time">{post.time}</span>
+                        </Link></b> <span>&#183;</span> <span className="time">{post.time}</span>
                     </div>
                     <div>{post.review}</div>
                 </div>
